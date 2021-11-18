1e4d6eca122beb547fdef07969e39916384a1f01
Rafi <t-rafi.abiyyi@traveloka.com>
2021-11-18 23:18:19 +0700

    fetch current longitude and latitude

--- lib/main.dart
+++ lib/main.dart
@@ -3,58 +3,28 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:geolocator/geolocator.dart';
+import 'utils/geo_helper.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: NinjaCard(),
+    home: HomeCard(),
   ));
 }
 
-class NinjaCard extends StatefulWidget {
-  const NinjaCard({Key? key}) : super(key: key);
+class HomeCard extends StatefulWidget {
+  const HomeCard({Key? key}) : super(key: key);
 
   @override
-  State<NinjaCard> createState() => _NinjaCardState();
+  State<HomeCard> createState() => _HomeCardState();
 }
 
-class _NinjaCardState extends State<NinjaCard> {
+class _HomeCardState extends State<HomeCard> {
 
   String datenow = DateFormat('dd MMMM yyyy').format(DateTime.now());
   String location ='Null, Press Button';
-  String address = 'search';
   String city = "Jakarta";
 
-  Future<Position> _getGeoLocationPosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      await Geolocator.openLocationSettings();
-      return Future.error('Location services are disabled.');
-    }
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-
-        return Future.error('Location permissions are denied');
-      }
-    }
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-  }
-
-
+  GeoHelper gh = GeoHelper();
 
   @override
   Widget build(BuildContext context) {
@@ -76,11 +46,11 @@ class _NinjaCardState extends State<NinjaCard> {
           // ),
           floatingActionButton: FloatingActionButton(
             onPressed: () async {
-              Position position = await _getGeoLocationPosition();
+              Position position = await gh.getLatLong();
               location ='Lat: ${position.latitude} , Long: ${position.longitude}';
               print(location);
               // setState((){
-              //   ninjaLevel += 1;
+              //   city = location;
               // });
             },
             child: Icon(Icons.add),
@@ -113,7 +83,7 @@ class _NinjaCardState extends State<NinjaCard> {
                 SizedBox(height: 10.0),
                 Center(
                   child: Image(
-                    image: NetworkImage('https://outrain-s3.s3.ap-southeast-1.amazonaws.com/png/c.png'),
+                    image: NetworkImage('https://outrain-s3.s3.ap-southeast-1.amazonaws.com/png/sn.png'),
                     height: 200,
                     width: 200,
                   ),
