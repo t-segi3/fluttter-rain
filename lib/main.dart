52935ccb18b4d874bfd14e81cf0dc74ee2fcac24
Rafi <t-rafi.abiyyi@traveloka.com>
2021-12-01 13:41:29 +0700

    navigation routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:geolocator/geolocator.dart';
-import 'utils/geo_helper.dart';
 
 import 'package:outrain/pages/home.dart';
 import 'package:outrain/pages/loading.dart';
+import 'package:outrain/pages/choose_location.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
   ));
 }
